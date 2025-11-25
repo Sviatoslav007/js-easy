b311d41372a6921dceb539e4cc1c652b157b8b06
Tolstosheiev Sviatoslav <sviatoslav.tolstosheev@gmail.com>
2025-11-25 13:25:31 +0200

    All tests were performed correctly

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,11 +1,12 @@
-// Задача: Написати функцію, яка приймає рядок і повертає його у зворотному порядку,
-//  при цьому пропускаючи всі цифри.
-
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  let result = "";
+  const digits = "0123456789";
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (!digits.includes(str[i])) {
+      result += str[i];
+    }
+  }
+  return result;
 }
-
-console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
-console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
 
 module.exports = reverseWithoutNumbers;
--- tasks/task3.js
+++ tasks/task3.js
@@ -1,11 +1,9 @@
-// Задача: Напишіть функцію sumArray, яка приймає масив чисел і 
-// повертає їхню суму. Використовуйте цикл для обчислення суми.
-
 function sumArray(numbers) {
-  // Ваш код тут
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
 }
-
-console.log(sumArray([1, 2, 3, 4, 5])); // Виведе: 15
-console.log(sumArray([10, 20, 30]));    // Виведе: 60
 
 module.exports = sumArray;
